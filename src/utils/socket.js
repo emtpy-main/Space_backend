b44338b40aeb9995c787e134585f2de40173b4e7
3pratik2 <singhpratik2210@gmail.com>
2025-10-13 01:13:06 +0530

    update

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -9,11 +9,23 @@ const initializeSocket = (server) => {
     }
   });
 
-  io.on("connection", (socket) => {
-    console.log("New client connected", socket.id);
+ io.on("connection", (socket) => {
+    socket.on('joinRoom', ({ fromUser, toUser }) => {
+      const roomId = [fromUser, toUser].sort().join("_");
+      socket.join(roomId);
+      console.log(`Socket ${socket.id} joined room: ${roomId}`);
+    }); 
+    
+    socket.on('sendMessage', (messagePayload) => {
+      // messagePayload = { from, to, text, ... }
+      const roomId = [messagePayload.from, messagePayload.to].sort().join("_");
+      
+      // Send the message to everyone in the room except the original sender
+      socket.to(roomId).emit('receiveMessage', messagePayload);
+    });
 
     socket.on("disconnect", () => {
-      console.log("Client disconnected", socket.id);
+      console.log(`Socket ${socket.id} disconnected`);
     });
   });
 };
